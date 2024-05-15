019c2a1959b9b76c2a12f575b08ddc644b3a538f
k-sekiya23 <158592486+k-sekiya23@users.noreply.github.com>
2024-05-15 18:06:00 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,40 @@
+'use strict';
+// SP用メニューバー
+{
+  const open = document.getElementById('open');
+  const overlay = document.querySelector('.overlay');
+  const close = document.getElementById('close');
+
+  open.addEventListener('click', () => {
+    overlay.classList.add('show');
+    open.classList.add('hide');
+  });
+
+  close.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+}
+
+
+// タブメニュー切り替えscript
+{
+  const menuItems = document.querySelectorAll('.menu li a');
+  const contents = document.querySelectorAll('.content');
+
+  menuItems.forEach(clickedItem => {
+    clickedItem.addEventListener('click', e => {
+      e.preventDefault();
+
+      menuItems.forEach(item => {
+        item.classList.remove('active');
+      });
+      clickedItem.classList.add('active');
+
+      contents.forEach(content => {
+        content.classList.remove('active');
+      });
+      document.getElementById(clickedItem.dataset.id).classList.add('active');
+    });
+  });
+}
